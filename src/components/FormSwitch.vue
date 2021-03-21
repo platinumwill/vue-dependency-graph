22eb89e538651b2cefeb27ece0a1e0178bbf5c14
platinumwill <platinumwill@gmail.com>
2021-03-21 13:01:58 +0800

    調整 code 縮排

--- src/components/FormSwitch.vue
+++ src/components/FormSwitch.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <button @click="openTranslationPatternWindow">Add Pattern Segment</button>
-<Dialog header="Header" v-model:visible="displayModal" :style="{width: '50vw'}" :modal="true">
-    <p class="p-m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</Dialog>
+        <Dialog header="Header" v-model:visible="displayModal" :style="{width: '50vw'}" :modal="true">
+            <p class="p-m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Dialog>
     </div>
 </template>
 
