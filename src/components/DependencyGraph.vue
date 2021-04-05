835294ad254ae6be620aa177e6b9b74e0cca7c8c
platinumwill <platinumwill@gmail.com>
2021-04-05 16:23:19 +0800

    TEMP OK; 新增 SentenceParseGraph

--- src/components/DependencyGraph.vue
+++ src/components/DependencyGraph.vue
@@ -1,17 +1,6 @@
 <template>
-    <div v-if="isDocumentReady">
-        
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:lang="en"
-        id="displacy-svg" class="displacy" :width="width" :height="height" 
-        :viewbox="viewbox" :data-format="config.format"
-        :style="{color: config.foregroundColor, background: config.backgroundColor, fontFamily: config.fontFamily}" 
-        preserveAspectRatio="xMinYMax meet">
-        <DependencyNode v-for="(word, index) in parse.words" :word="word" :index="index" :key="index" :config="config"></DependencyNode>
-        <DependencyEdge v-for="arc in parse.arcs" :arc="arc" :key="arc.start + '_to_' + arc.end" :config="config"></DependencyEdge>
-    </svg>
 
-    </div>
-
+    <SentenceParseGraph></SentenceParseGraph>
     <DocumentInput></DocumentInput>
     <PatternDialog></PatternDialog>
     <DocumentPanel></DocumentPanel>
@@ -19,18 +8,15 @@
 </template>
 
 <script>
-import DependencyEdge from "./DependencyEdge.vue";
-import DependencyNode from "./DependencyNode.vue";
 import DocumentPanel from "./DocumentPanel.vue"
 import DocumentInput from "./DocumentInput.vue"
 import PatternDialog from "./PatternDialog.vue"
-import { mapGetters } from 'vuex'
+import SentenceParseGraph from "./SentenceParseGraph.vue"
 
 export default {
   name: 'DependencyGraph'
   , components: {
-      DependencyEdge
-      , DependencyNode
+      SentenceParseGraph
       , DocumentPanel
       , DocumentInput
       , PatternDialog
@@ -42,24 +28,6 @@ export default {
             return {}
         }
     }
-    , config: {
-        type: Object
-        , default: function() {
-            return {
-                distance: 200
-                , offsetX: 50
-                , arrowSpacing: 20
-                , arrowStroke: 2
-                , wordSpacing: 75
-                , format: 'spacy'
-                , foregroundColor: '#ff0000'
-                , selectedForegroundColor: '#00ff00'
-                , backgroundColor: '#000000'
-                , fontFamily:'inherit' 
-                , arrowWidth: 10 
-            }
-      }
-    }
     , provided() {
         return {
             config: this.config
@@ -70,33 +38,6 @@ export default {
     return {
       //
     } 
-  }
-  , computed: {
-    levels: function() {
-        return [...new Set(this.parse.arcs.map(({ end, start }) => end - start).sort((a, b) => a - b))]
-    }
-    , highestLevel: function() {
-        return this.levels.indexOf(this.levels.slice(-1)[0]) + 1
-    } 
-    , width: function() {
-        return this.config.offsetX + this.parse.words.length * this.config.distance
-    }
-    , height: function() {
-        return this.offsetY + 3 * this.config.wordSpacing
-    } 
-    , viewbox: function() {
-        return "0 0 " + this.width + " " + this.height
-    }
-    , offsetY: function() {
-        return this.config.distance / 2 * this.highestLevel
-    }
-    , ...mapGetters([
-        'documentParse'
-        , 'isDocumentReady'
-        , 'maxSentenceIndex'
-    ])
-}
-  , methods: {
   }
 }
 </script>
---
+++ src/components/SentenceParseGraph.vue
@@ -0,0 +1,69 @@
+<template>
+    <div v-if="isDocumentReady">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:lang="en"
+            id="displacy-svg" class="displacy" :width="width" :height="height" 
+            :viewbox="viewbox" :data-format="config.format"
+            :style="{color: config.foregroundColor, background: config.backgroundColor, fontFamily: config.fontFamily}" 
+            preserveAspectRatio="xMinYMax meet">
+            <DependencyNode v-for="(word, index) in parse.words" :word="word" :index="index" :key="index" :config="config"></DependencyNode>
+            <DependencyEdge v-for="arc in parse.arcs" :arc="arc" :key="arc.start + '_to_' + arc.end" :config="config"></DependencyEdge>
+        </svg>
+    </div>
+</template>
+
+<script>
+import DependencyEdge from "./DependencyEdge.vue";
+import DependencyNode from "./DependencyNode.vue";
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        levels: function() {
+            return [...new Set(this.parse.arcs.map(({ end, start }) => end - start).sort((a, b) => a - b))]
+        }
+        , highestLevel: function() {
+            return this.levels.indexOf(this.levels.slice(-1)[0]) + 1
+        } 
+        , width: function() {
+            return this.config.offsetX + this.parse.words.length * this.config.distance
+        }
+        , height: function() {
+            return this.offsetY + 3 * this.config.wordSpacing
+        } 
+        , viewbox: function() {
+            return "0 0 " + this.width + " " + this.height
+        }
+        , offsetY: function() {
+            return this.config.distance / 2 * this.highestLevel
+        }
+        , ...mapGetters({ 
+            isDocumentReady: 'isDocumentReady'
+            , parse: 'documentParse'
+        })
+    }
+    , props: {
+        config: {
+            type: Object
+            , default: function() {
+                return {
+                    distance: 200
+                    , offsetX: 50
+                    , arrowSpacing: 20
+                    , arrowStroke: 2
+                    , wordSpacing: 75
+                    , format: 'spacy'
+                    , foregroundColor: '#ff0000'
+                    , selectedForegroundColor: '#00ff00'
+                    , backgroundColor: '#000000'
+                    , fontFamily:'inherit' 
+                    , arrowWidth: 10 
+                }
+        }
+        }
+    }
+    , components: {
+        DependencyEdge
+        , DependencyNode
+    } 
+}
+</script>
