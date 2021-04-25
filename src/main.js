889c71c0d4f7e3cf8615570aeaceb7e0400002bf
platinumwill <platinumwill@gmail.com>
2021-04-25 22:15:48 +0800

    stanfordnlp server 改在 ubuntu 執行

--- src/main.js
+++ src/main.js
@@ -38,7 +38,8 @@ const store = createStore({
 
       // Stanford CoreNLP
       // properties={"annotators":"tokenize,pos,parse,lemma","outputFormat":"json"}
-      await axios.post('http://localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
+      // await axios.post('http://localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
+      await axios.post('http://172.22.102.238:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
         console.log(response.data);
       }).catch(function(error) {
         console.log(error)
