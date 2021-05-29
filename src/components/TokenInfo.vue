727b6339a081ddd507891f1b5a0dd0300db706af
platinumwill <platinumwill@gmail.com>
2021-05-29 11:29:04 +0800

    移動句子的時候重設選取狀態

--- src/components/TokenInfo.vue
+++ src/components/TokenInfo.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     name: 'DependencyLemma'
     , inject: ['config', 'tokenIndex']
@@ -22,10 +24,18 @@ export default {
             this.selected = !this.selected
         }
     }
+    , watch: {
+        currentSentenceIndex () {
+            this.selected = false
+        }
+    }
     , computed: {
         color: function() {
             return this.selected ? this.config.selectedForegroundColor : 'currentColor'
         }
+        , ...mapGetters([
+            'currentSentenceIndex'
+            ])
 
     }
 }
