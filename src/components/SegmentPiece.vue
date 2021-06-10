b5e7921b60818553450a4fd86264a7397843c11f
platinumwill <platinumwill@gmail.com>
2021-06-10 23:08:21 +0800

    Autocomplete option group 試用 ok

--- src/components/SegmentPiece.vue
+++ src/components/SegmentPiece.vue
@@ -8,7 +8,15 @@
         </template>
         <template #content>
             <div v-if="item.type == 'Lemma'">
-                <AutoComplete>
+                <AutoComplete 
+                    :suggestions="filteredDictionaries"
+                    field="target"
+                    optionGroupChildren="entries"
+                    optionGroupLabel="label"
+                    @complete="searchInDictionaries($event)"
+                    v-model="appliedText" 
+                    :dropdown="true"
+                    :inputStyle="{'width':'50%'}">
                 </AutoComplete>
             </div>
         </template>
@@ -24,13 +32,29 @@
 <script>
 import Card from 'primevue/card'
 import Button from 'primevue/button'
-import AutoComplete from 'primevue/autocomplete';
+import AutoComplete from 'primevue/autocomplete'
+import {FilterService, FilterMatchMode} from 'primevue/api'
 
 export default {
     components: {
         Card
         , Button
         , AutoComplete
+    }
+    , data() {
+        return {
+            appliedText: undefined
+            , mockDictionaries: [
+                {
+                    label: '自訂字典'
+                    , entries: [
+                        {source: 'send', target: '送'}
+                        , {source: 'send', target: '寄送'}
+                    ]
+                }
+            ]
+            , filteredDictionaries: []
+        }
     }
     , props: {
         item: {
@@ -40,6 +64,17 @@ export default {
     , methods: {
         removeSelf: function () {
             this.$emit('removePiece', this.item)
+        }
+        , searchInDictionaries(event) {
+            let query = event.query
+            let tempFilteredDictionaries = []
+            for (let dictionary of this.mockDictionaries) {
+                let filteredEntries = FilterService.filter(dictionary.entries, ['target'], query, FilterMatchMode.CONTAINS)
+                if (filteredEntries && filteredEntries.length) {
+                    tempFilteredDictionaries.push({...dictionary, ...{entries: filteredEntries}})
+                }
+            }
+            this.filteredDictionaries = tempFilteredDictionaries
         }
     }
 }
