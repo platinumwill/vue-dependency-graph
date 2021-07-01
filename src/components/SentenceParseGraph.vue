85fc44a3727d3525cfa4629764f576f304d28e49
platinumwill <platinumwill@gmail.com>
2021-07-01 22:22:12 +0800

    composition api 拆出獨立檔案也 OK 了

--- src/components/SentenceParseGraph.vue
+++ src/components/SentenceParseGraph.vue
@@ -16,8 +16,16 @@
 import DependencyEdge from "./DependencyEdge.vue";
 import DependencyNode from "./DependencyNode.vue";
 import { mapGetters } from 'vuex'
-import { provide, ref } from 'vue'
+import { provide } from 'vue'
 import PatternDialog from "./PatternDialog.vue"
+import {
+    selectedPOSs
+    , selectedLemmas
+    , selectedDependencies
+    , togglePOSSelected
+    , toggleLemmaSelected
+    , toggleDependencySelected
+} from "@/composables/selectionManager"
 
 export default {
     data() {
@@ -136,40 +144,6 @@ export default {
         , PatternDialog
     } 
     , setup() {
-        const selectedPOSs = ref([])
-        const selectedLemmas = ref([])
-        const selectedDependencies = ref([])
-
-        const togglePOSSelected = (posIndex) => {
-            const indexOfPOS = selectedPOSs.value.indexOf(posIndex)
-            if (indexOfPOS >= 0) {
-                selectedPOSs.value.splice(indexOfPOS, 1)
-            } else {
-                selectedPOSs.value.push(posIndex)
-            }
-            if (selectedLemmas.value.indexOf(posIndex) >= 0) {
-                selectedLemmas.value.splice(selectedLemmas.value.indexOf(posIndex), 1)
-            }
-        }
-        const toggleLemmaSelected = (lemmaIndex) => {
-            const indexOfLemma = selectedLemmas.value.indexOf(lemmaIndex)
-            if (indexOfLemma >= 0) {
-                selectedLemmas.value.splice(indexOfLemma, 1)
-            } else {
-                selectedLemmas.value.push(lemmaIndex)
-            }
-            if (selectedPOSs.value.indexOf(lemmaIndex) >= 0) {
-                selectedPOSs.value.splice(indexOfLemma, 1)
-            }
-        }
-        const toggleDependencySelected = (dependencyIndex) => {
-            const indexOfDependency = selectedDependencies.value.indexOf(dependencyIndex)
-            if (indexOfDependency >= 0) {
-                selectedDependencies.value.splice(indexOfDependency, 1)
-            } else {
-                selectedDependencies.value.push(dependencyIndex)
-            }
-        }
 
         // provide('posManager', {selections: selectedPOSs, toggler: togglePOSSelected})
         // provide('lemmaManager', {selections: selectedLemmas, toggler: toggleLemmaSelected})
