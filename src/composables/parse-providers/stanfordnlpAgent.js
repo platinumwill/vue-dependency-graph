ce20a81fcd860b84dcb1aadb40c4758c8957460b
platinumwill <platinumwill@gmail.com>
2021-07-10 16:15:22 +0800

    從 stanfordnlpAgent 拆出 stanford-api

--- src/composables/parse-providers/stanfordnlpAgent.js
+++ src/composables/parse-providers/stanfordnlpAgent.js
@@ -1,17 +1,14 @@
-import axios from 'axios'
+import stanfordnlpApi from '@/composables/stanfordnlp-api'
+
 export default async function (documentText) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            // Stanford CoreNLP
-            // properties={"annotators":"tokenize,pos,parse,lemma","outputFormat":"json"}
-            // await axios.post('http://localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
-            axios.post('http://stanford-local:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
+            stanfordnlpApi(documentText).then((stanfordParse) => {
                 console.log("STANFORD parse:")
-                const stanfordParse = response.data
+                console.log(stanfordParse)
                 // 手動把 sentenceIndex 加到 dependency 裡面
                 stanfordParse.sentences.forEach(sentence => sentence.tokens.forEach(token => token.sentenceIndex = sentence.index))
                 stanfordParse.sentences.forEach(sentence => sentence.enhancedDependencies.forEach(dependency => dependency.sentenceIndex = sentence.index))
-                console.log(stanfordParse)
                 const tokens = stanfordParse.sentences.flatMap( (sentence) => sentence.tokens )
                 const dependencies = stanfordParse.sentences.flatMap( (sentence) => sentence.enhancedDependencies )
                 function newInfoDependency(dependency) {
@@ -31,7 +28,7 @@ export default async function (documentText) {
                 resolve(stanfordSpacyFormatParse)
                 console.log("STANFORD parse in Spacy format:")
                 console.log(stanfordSpacyFormatParse)
-            }).catch(function(error) {
+            }).catch((error) => {
                 console.log(error)
                 reject(error)
             })
---
+++ src/composables/stanfordnlp-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export default async function (documentText) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // Stanford CoreNLP
+            // properties={"annotators":"tokenize,pos,parse,lemma","outputFormat":"json"}
+            // await axios.post('http://localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
+            axios.post('http://stanford-local:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D'
+                , documentText)
+            .then(function(response) {
+                resolve(response.data)
+            }).catch(function(error) {
+                console.log(error)
+                reject(error)
+            })
+        }, 1000)
+    })
+}
