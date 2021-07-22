908bc0d4ccd55d52b064539280983c642743a6d1
platinumwill <platinumwill@gmail.com>
2021-07-22 19:56:22 +0800

    調整 stanfordnlp 的 properties

--- src/composables/api/stanfordnlp-api.js
+++ src/composables/api/stanfordnlp-api.js
@@ -4,8 +4,8 @@ export default async function (documentText) {
     return new Promise((resolve, reject) => {
             // Stanford CoreNLP
             // properties={"annotators":"tokenize,pos,parse,lemma","outputFormat":"json"}
-            // await axios.post('http://localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D', documentText).then(function(response) {
-            axios.post('http://stanford-local:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cpos%2Cparse%2Clemma%22%2C%22outputFormat%22%3A%22json%22%7D'
+            const encodedQueryString = encodeURI('?properties={"annotators": "tokenize,ssplit,pos,ner,depparse,openie"}&pipelineLanguage=en')
+            axios.post('http://stanford-local:9000/' + encodedQueryString
                 , documentText)
             .then(function(response) {
                 resolve(response.data)
