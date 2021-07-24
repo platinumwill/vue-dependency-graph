115324f477f635e519a61f65dd74f51ce0752bc4
platinumwill <platinumwill@gmail.com>
2021-07-24 10:03:45 +0800

    google api 改呼叫 annotateText

--- src/composables/api/google-api.js
+++ src/composables/api/google-api.js
@@ -1,14 +1,22 @@
 import axios from 'axios'
 export default async function (documentText) {
     return new Promise((resolve, reject) => {
-            const google_url = 'https://language.googleapis.com/v1/documents:analyzeSyntax?key=AIzaSyAxueNH_QGMkUSVBse8VSzfOTUUZ1oRfxM';
+            const google_url = 'https://language.googleapis.com/v1/documents:annotateText?key=AIzaSyAxueNH_QGMkUSVBse8VSzfOTUUZ1oRfxM';
             let google_param = {};
             let document = {};
             document.type = 'PLAIN_TEXT'
             document.language = 'en'
             document.content = documentText
+            const features = {
+}
+            features.extractSyntax = true
+            features.extractEntities = true
+            features.classifyText = false
+
             google_param.document = document
             google_param.encodingType = 'UTF8'
+            google_param.features = features
+
             axios.post(google_url, google_param).then(function(response) {
                 resolve(response.data)
             }).catch(function(error) {
