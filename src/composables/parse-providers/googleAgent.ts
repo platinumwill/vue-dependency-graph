27d0f85a85cd843f94cf7924dd727e1b9820afac
platinumwill <platinumwill@gmail.com>
2021-12-27 15:55:10 +0800

    googleAgent 改成 ts

---
+++ src/composables/parse-providers/googleAgent.ts
@@ -0,0 +1,25 @@
+import googleApi from "@/composables/api/google-api"
+export default async function (documentText: string) {
+    return new Promise((resolve, reject) => {
+            googleApi(documentText).then((parse) => {
+                console.log("GOOGLE parse:")
+                console.log(parse)
+                const googleParseConvertedSpacy = ({
+                            arcs: parse.tokens.map(
+                                        ( { dependencyEdge: { label, headTokenIndex: j }}: any, i: number ) => 
+                                            (i != j)
+                                            ? ({ label, start: Math.min(i, j), end: Math.max(i, j), dir: (j > i) ? 'left' : 'right' }) 
+                                            : null
+                                    )
+                                    .filter((word: any): boolean => word != null)
+                            , words: parse.tokens.map(({ text: { content: text }, partOfSpeech: { tag, tense }, lemma: lemma }: any ) => ({ text, tag, tense, lemma }))
+                        })
+                console.log("GOOGLE parse in Spacy format:")
+                console.log(googleParseConvertedSpacy)
+                resolve(googleParseConvertedSpacy)
+            }).catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
--- src/composables/parse-providers/parseProviderManager.js
+++ src/composables/parse-providers/parseProviderManager.js
@@ -1,6 +1,6 @@
 import spacy from '@/composables/parse-providers/spacyAgent.js'
 import stanford from '@/composables/parse-providers/stanfordnlpAgent.js'
-import google from '@/composables/parse-providers/googleAgent.js'
+import google from '@/composables/parse-providers/googleAgent'
 
 export default function() {
     return {
