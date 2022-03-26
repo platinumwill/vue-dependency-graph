12d66646ac91350ac313ebd894a38572f5d77da2
platinumwill <platinumwill@gmail.com>
2022-03-26 15:22:27 +0800

    新增 target pattern 按鈕和對話框

--- src/components/SegmentDialog.vue
+++ src/components/SegmentDialog.vue
@@ -8,6 +8,7 @@
     </Button>
 
     <OverlayPanel ref="panel">
+
         <!-- source segment 下拉選單 -->
         <Dropdown v-model="sourceSegment.selection.selectedPattern"
             :options="sourceSegment.selection.options.value"
@@ -16,8 +17,28 @@
             :showClear="true"
             >
         </Dropdown>
+
         <br/>
+        <Button
+            @click="toggleTargetPatternDialog"
+            >
+            Target Pattern...
+        </Button>
+
     </OverlayPanel>
+
+    <!-- target pattern 對話框 -->
+    <Dialog
+        v-model:visible="showTargetPatternDialog" 
+        :maximizable="true"
+        :keepInViewport="false"
+        :style="{width: '100vw'}" :modal="true" :closeOnEscape="true" position="topleft"
+        >
+        <template #header>
+            <h2>Target Pattern</h2>
+
+        </template>
+    </Dialog>
     
 </template>
 
@@ -26,6 +47,7 @@
 import { defineComponent, ref, computed, inject, ComputedRef, toRef } from 'vue'
 
 import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
 import OverlayPanel from 'primevue/overlaypanel'
 import Dropdown from 'primevue/dropdown'
 
@@ -59,15 +81,24 @@ export default defineComponent({
             panel.value.toggle(event)
         }
 
+        // target pattern dialog
+        const showTargetPatternDialog = ref<Boolean>(false)
+        const toggleTargetPatternDialog = () => {
+            showTargetPatternDialog.value = !showTargetPatternDialog.value
+        }
+
         return {
             display
             , panel
             , togglePanel
             , sourceSegment: props.token.segmentHelper
+            , showTargetPatternDialog
+            , toggleTargetPatternDialog
         }
     }
     , components: {
         Button
+        , Dialog
         , Dropdown
         , OverlayPanel
     }
