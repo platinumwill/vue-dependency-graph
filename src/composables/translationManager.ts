4f3931cbfa263677aa63548e190da7b8c64e8080
platinumwill <platinumwill@gmail.com>
2022-03-31 12:56:19 +0800

    從 pattern logic 把 saveSelectedPattern 複製過來，然後調整

--- src/composables/translationManager.ts
+++ src/composables/translationManager.ts
@@ -1,10 +1,41 @@
 import { SourcePatternManager } from "@/composables/sourcePatternManager";
 import { TargetPattern } from "@/composables/targetPatter";
+import { aliases, GremlinInvoke, submit } from "@/composables/gremlinManager";
+
+export type TranslationManager = {
+    saveSelectedPattern: Function
+}
 
 export function translationManager(
     sourcePattern: SourcePatternManager
     , targetPattern: TargetPattern
 ) {
 
+    return {
+        saveSelectedPattern: saveSelectedPattern
+    }
 }
 
+const saveSelectedPattern = (
+    sourcePattern: SourcePatternManager
+    , targetPattern: TargetPattern
+) => {
+    let gremlinInvoke = new GremlinInvoke()
+
+    gremlinInvoke = sourcePattern.process.save(gremlinInvoke)
+    gremlinInvoke = targetPattern.process.save(gremlinInvoke)
+    gremlinInvoke.call("select", aliases.sourcePatternBeginning)
+
+    console.log(gremlinInvoke.command())
+    submit(gremlinInvoke.command())
+    .then((resultData: any) => {
+        const sourcePatternBeginningVertexId = resultData['@value'][0]['@value'].id['@value']
+        console.log('Source Pattern Begin Vertex Id: ', sourcePatternBeginningVertexId)
+        sourcePattern.selection.reloadOptions().then(() => {
+            sourcePattern.selection.setAsSelected(sourcePatternBeginningVertexId)
+        })
+        return sourcePatternBeginningVertexId
+    }).catch(function(error) {
+        console.error(error)
+    })
+}
