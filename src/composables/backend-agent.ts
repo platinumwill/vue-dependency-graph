0bfa7f6530c648367139ee62195ecaa0f09a1efe
platinumwill <platinumwill@gmail.com>
2023-07-17 13:45:45 +0800

    調整 invoke url 用法；移除註解

--- src/composables/backend-agent.ts
+++ src/composables/backend-agent.ts
@@ -1,7 +1,7 @@
 const apigClientFactory = require('aws-api-gateway-client').default;
 
 const config = {
-    invokeUrl: 'https://' + process.env.VUE_APP_AWS_API_INVOKE_URL
+    invokeUrl: process.env.VUE_APP_AWS_API_INVOKE_URL
     , region: process.env.VUE_APP_AWS_REGION
     , accessKey: process.env.VUE_APP_AWS_ACCESSKEY
     , secretKey: process.env.VUE_APP_AWS_SECRETKEY
@@ -13,7 +13,6 @@ const pathParams = {}
 const pathTemaplte = ''
 const method = 'POST'
 const additionalParams = {}
-// https://jqjs9epd00.execute-api.ap-southeast-1.amazonaws.com/prod
 export async function queryExistingDocument(documentId: number|undefined, documentText: string|undefined) {
     console.log('function, apigclientFactory', apigClientFactory)
     const document = {
